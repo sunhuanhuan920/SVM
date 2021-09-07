8694489473da735ac516f6240333f293409f18b5
Zhenhuann Sun <sunsteven11@gmail.com>
2021-09-07 16:57:03 -0400

    Updated svm.py

---
+++ svm.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cvxopt as cx
+
+class svm:
+    def __init_(self, kernel="polynomial", degree=2, C=1):
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
